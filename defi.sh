4ea577c1fece31125a0933810857ae251479810f
Unknown <developpix@gmail.com>
2017-11-09 17:39:55 +0100

    Change control of invisible caracters
    Verify with REGEX if the ligne is in empty form or not

--- defi.sh
+++ defi.sh
@@ -23,67 +23,23 @@ do
 		for ligne in $(cat "$fichier" | cut -d '#' -f 1)
 		do
 		
-			# On suppose que la ligne est vide
+			# On suppose que la ligne est vide de base
 			vide=true
 			
-			# Longueur de la ligne en caracteres
-			longueur=$(expr length $ligne)
+			# Si contient autre chose que espace, tabulation et les espaces insécables
+			# ancien à la fin du grep entre crochets $'\x01'$'\x02'
+			# Maintenant retire caractères de control [:cntrl:] en les transformants en espace insécable le temps du test de la ligne
+			# Maintenant retire les blancs [:blank:] en les transformants en espace le temps du test de la ligne
+			contient=$(echo $ligne | tr [:blank:] " " | grep -e [^" ""	"" "" "])
+			if [[ $contient != "" ]]
+			then
 			
-			# Position du caractere initialiser a 1
-			pos=1
-			
-			# Parcourir la ligne caractere par caractere
-			while [ $pos -le $longueur ]
-			do
-			
-				# On recupere le caractere
-				char=$(expr substr $ligne $pos 1)
-				# On recupere la valeur hexadecimal du caractere
-				DebutHex=$(echo $char | xxd | cut -c 11 )
+				vide=false
 				
-				let posSuivant=pos+1
-				
-				if [ $DebutHex = "c" ] || [ $DebutHex = "d" ]
-				then
-					
-					char=$(expr substr $ligne $pos 2)
-					let posSuivant=pos+2
-					
-				fi
-				
-				if [ $DebutHex = "e" ]
-				then
-					
-					char=$(expr substr $ligne $pos 3)
-					let posSuivant=pos+3
-					
-				fi
-				
-				if [ $DebutHex = "f" ]
-				then
-					
-					char=$(expr substr $ligne $pos 4)
-					let posSuivant=pos+4
-					
-				fi
-				
-				hex=$(echo $char | xxd)
-				
-				# Si le caractere est different de espace ou de tab ou espace insecable
-				if [ $hex != "00000000: 200a                                      ." ] && [ $hex != "00000000: 090a                                     .." ] && [ $hex != "00000000: e280 af0a                                ...." ] && [ $hex != "00000000: c2a0 0a                                  ..." ]
-				then
-				
-					vide=false
-				
-				fi
-				
-				# On incremente la position de 1
-				pos=$posSuivant
-				
-			done
+			fi
 			
 			# Forme de la ligne
-			FORME='^[on]:[0-9]{3}[\:][a-z]+[\:][0-9]+[\:][0-9]+[" "]*$'
+			FORME='^[on]:[0-9]{3}[\:][a-z]+[\:][0-9]+[\:][0-9]+[" ""	"" "" "]*$'
 			
 			# Si le ligne 2 ou sup et ligne de la forme
 			if [ $numeroLigne -ge 2 ] && [[ $ligne =~ $FORME ]]
@@ -119,64 +75,20 @@ do
 				for ligne in $(cat "$fichier" | cut -d '#' -f 1)
 				do
 				
-					# On suppose que la ligne est vide
+					# On suppose que la ligne est vide de base
 					vide=true
 					
-					# Longueur de la ligne en caracteres
-					longueur=$(expr length $ligne)
+					# Si contient autre chose que espace, tabulation et les espaces insécables
+					# ancien à la fin du grep entre crochets $'\x01'$'\x02'
+					# Maintenant retire caractères de control [:cntrl:] en les transformants en espace insécable le temps du test de la ligne
+					# Maintenant retire les blancs [:blank:] en les transformants en espace le temps du test de la ligne
+					contient=$(echo $ligne | tr [:cntrl:] " " | tr [:blank:] " " | grep -e [^" ""	"" "" "])
+					if [[ $contient != "" ]]
+					then
 					
-					# Position du caractere initialiser a 1
-					pos=1
-					
-					# Parcourir la ligne caractere par caractere
-					while [ $pos -le $longueur ]
-					do
-					
-						# On recupere le caractere
-						char=$(expr substr $ligne $pos 1)
-						# On recupere la valeur hexadecimal du caractere
-						DebutHex=$(echo $char | xxd | cut -c 11 )
+						vide=false
 						
-						let posSuivant=pos+1
-						
-						if [ $DebutHex = "c" ] || [ $DebutHex = "d" ]
-						then
-							
-							char=$(expr substr $ligne $pos 2)
-							let posSuivant=pos+2
-							
-						fi
-						
-						if [ $DebutHex = "e" ]
-						then
-							
-							char=$(expr substr $ligne $pos 3)
-							let posSuivant=pos+3
-							
-						fi
-						
-						if [ $DebutHex = "f" ]
-						then
-							
-							char=$(expr substr $ligne $pos 4)
-							let posSuivant=pos+4
-							
-						fi
-						
-						hex=$(echo $char | xxd)
-						
-						# Si le caractere est different de espace ou de tab ou espace insecable
-						if [ $hex != "00000000: 200a                                      ." ] && [ $hex != "00000000: 090a                                     .." ] && [ $hex != "00000000: e280 af0a                                ...." ] && [ $hex != "00000000: c2a0 0a                                  ..." ]
-						then
-						
-							vide=false
-						
-						fi
-						
-						# On incremente la position de 1
-						pos=$posSuivant
-						
-					done
+					fi
 					
 					# Si la ligne n est pas vide
 					if [ $vide = false ]
