8876dfa7474ea3aae16c55630411b17529030003
Unknown <developpix@gmail.com>
2017-11-11 10:28:18 +0100

    Code correction and language correction
    Transform : in [\:]
    Fix and add commentary

--- defi.sh
+++ defi.sh
@@ -7,6 +7,7 @@ IFS=$'\n'
 for fichier in $(ls -a --file-type | grep -v "/" | grep -v "@")
 do
 
+	# Recupere les 5 premiers caracteres de la premiere ligne du fichier
 	debutFichier=$(head -n 1 "$fichier" | cut -c -5)
 	
 	# Si le debut du fichier est data:
@@ -26,27 +27,27 @@ do
 			# On suppose que la ligne est vide de base
 			vide=true
 			
-			# Si contient autre chose que espace, tabulation et les espaces insécables
-			# ancien à la fin du grep entre crochets $'\x01'$'\x02'
-			# Maintenant retire caractères de control [:cntrl:] en les transformants en espace insécable le temps du test de la ligne
-			# Maintenant retire les blancs [:blank:] en les transformants en espace le temps du test de la ligne
-			contient=$(echo $ligne | tr [:cntrl:] " " | tr [:blank:] " " | grep -e [^" "$'\xe2\x80\xaf'$'\xc2\xa0'])
+			# Si contient autre chose que caracteres de controle, espace, tabulation et les espaces insécables ou autre blanc horizontaux
+			# Retire les caractères de control [:cntrl:] en les transformants en espace le temps du test de la ligne
+			# Retire les blancs [:blank:] en les transformants en espace le temps du test de la ligne
+			contient=$(echo $ligne | tr [:cntrl:] " " | tr [:blank:] " " | grep -e [^" "$'\xe2\x80\xaf'$'\xc2\xa0'])
 			if [[ $contient != "" ]]
 			then
 			
+				# La ligne n est pas vide
 				vide=false
 				
 			fi
 			
 			# Forme de la ligne
-			FORME='^[on]:[0-9]{3}[\:][a-z]+[\:][0-9]+[\:][0-9]+$'
+			FORME='^[on][\:][0-9]{3}[\:][a-z]+[\:][0-9]+[\:][0-9]+$'
 			
-			# Si le ligne 2 ou sup et ligne de la forme
+			# Si la ligne 2 ou sup et ligne est de la bonne forme
 			if [ $numeroLigne -ge 2 ] && [[ $ligne =~ $FORME ]]
 			then
 				
+				# Ne pas afficher
 				afficher=$afficher
-				#echo "bien formé"
 				
 			else
 			# Sinon
@@ -55,8 +56,8 @@ do
 				if [ $numeroLigne -ge 2 ] && [ $vide = false ]
 				then
 					
+					# Ne pas afficher
 					afficher=false
-					#echo "mal formé"
 					
 				fi
 				
@@ -69,6 +70,7 @@ do
 		if [ $afficher = true ]
 		then
 				
+				# Afficher le nom du fichier en minuscule
 				echo "`echo $fichier | tr '[:upper:]' '[:lower:]'`:"
 				
 				# Parcourir les lignes du fichier pour vérifier sa structure
@@ -78,11 +80,10 @@ do
 					# On suppose que la ligne est vide de base
 					vide=true
 					
-					# Si contient autre chose que espace, tabulation et les espaces insécables
-					# ancien à la fin du grep entre crochets $'\x01'$'\x02'
-					# Maintenant retire caractères de control [:cntrl:] en les transformants en espace insécable le temps du test de la ligne
-					# Maintenant retire les blancs [:blank:] en les transformants en espace le temps du test de la ligne
-					contient=$(echo $ligne | tr [:cntrl:] " " | tr [:blank:] " " | grep -e [^" "$'\xe2\x80\xaf'$'\xc2\xa0'])
+					# Si contient autre chose que caracteres de controle, espace, tabulation et les espaces insécables ou autre blanc horizontaux
+					# Retire les caractères de control [:cntrl:] en les transformants en espace le temps du test de la ligne
+					# Retire les blancs [:blank:] en les transformants en espace le temps du test de la ligne
+					contient=$(echo $ligne | tr [:cntrl:] " " | tr [:blank:] " " | grep -e [^" "$'\xe2\x80\xaf'$'\xc2\xa0'])
 					if [[ $contient != "" ]]
 					then
 					
